73790ecacec64e86b05693de728ba466b9bf6f93
ali <alibalaei76@gmail.com>
2021-04-22 02:11:18 +0430

    project started

---
+++ doodle/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              "doodle",
+              style: TextStyle(fontSize: 50, color: Colors.red[300]),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                hintText: 'username',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'password',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Text("log in"),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
